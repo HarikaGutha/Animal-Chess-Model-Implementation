b3f4b4183eddcfe0eb61b292b94f46e636947c4b
190026870 <hg64@st-andrews..ac.uk>
2020-02-10 15:17:44 +0000

    modidfied logic checkCapturingMovesAtBottomLeftCornerOfTheBoard() test passed

--- src/main/java/stacs/arcade/reversi/ReversiModel.java
+++ src/main/java/stacs/arcade/reversi/ReversiModel.java
@@ -85,25 +85,27 @@ public class ReversiModel {
     private void rejectMovesThatDoNotCapture(PlayerColour player, int x, int y) throws IllegalMoveException {
         if (x < 1 && y < 1) {
             if ((getAt(x, y + 1) != null) || (getAt(x + 1, y) != null) || getAt(x + 1, y + 1) != null) {
-                commonCode(player, x, y);
+                commonCode(player,x,y);
             }
         } else if ((x > 1 && x < HEIGHT - 2) && y <= 1) {
             if ((getAt(x, y + 1) != null) || (getAt(x + 1, y) != null) || (getAt(x + 1, y + 1) != null)
                     || (getAt(x - 1, y) != null) || (getAt(x - 1, y + 1) != null)) {
-                commonCode(player, x, y);
+                commonCode(player,x,y);
             }
-        } else {
+        } else if ((x >= HEIGHT - 2 && x <= HEIGHT - 1) && y <= 1) {
+            if ((getAt(x - 1, y) != null) || (getAt(x - 1, y + 1) != null) || (getAt(x, y + 1) != null)) {
+                commonCode(player,x,y);
+            }
+        }
+        else {
 
-            if ((getAt(x - 1, y - 1) != null) || (getAt(x - 1, y) != null) || (getAt(x - 1, y + 1) != null)
-                    || (getAt(x, y - 1) != null) || (getAt(x, y + 1) != null) || (getAt(x + 1, y - 1) != null)
-                    || (getAt(x + 1, y) != null) || (getAt(x + 1, y + 1) != null)) {
-                commonCode(player, x, y);
+            if ((getAt(x - 1, y - 1) != null) || (getAt(x - 1, y) != null) || (getAt(x - 1, y + 1) != null) || (getAt(x, y - 1) != null) || (getAt(x, y + 1) != null) || (getAt(x + 1, y - 1) != null) || (getAt(x + 1, y) != null) || (getAt(x + 1, y + 1) != null)) {
+                commonCode(player,x,y);
             } else {
                 throw new IllegalMoveException("invalid move");
             }
         }
     }
-
     private void commonCode(PlayerColour player, int x, int y) throws IllegalMoveException {
         board[x][y] = player;
         executeCapturingMoves(player, x, y);
